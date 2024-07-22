ce3236053e07ec45afde00ea1d32d875fe42fe95
zhou6579 <zhang.8678@osu.edu>
2024-07-22 01:52:48 -0400

    code update

--- alg2.py
+++ alg2.py
@@ -3,7 +3,7 @@ import alg1_parameters as parameters
 import random
 import matplotlib.pyplot as plt 
 
-def alg1():
+def alg2_form():
 
 	G = nx.Graph()
 
@@ -14,8 +14,6 @@ def alg1():
 	quorum_sets = parameters.QUORUM_SETS.copy()
 
 	epoch_counter = 0
-	bump_ups = 0
-	last_bumpup_at = 0
 
 	while True:
 		random.shuffle(all_validators)
@@ -27,14 +25,15 @@ def alg1():
 			if len(list(nx.all_neighbors(G, cur_validator))) >= quorum_sets[cur_validator][0]:
 				satisfied_counter += 1
 				if satisfied_counter == parameters.TOTAL_NUM_OF_VALIDATORS:
+					plt.figure(1)
 					nx.draw_networkx(G, with_labels=True)
 					plt.show()
-					return "CONVERGED, " + "EPOCH: " + str(epoch_counter) + " " + "SATISFIED: " + str(satisfied_counter)
+					return "CONVERGED, " + "EPOCH: " + str(epoch_counter) + " " + "SATISFIED: " + str(satisfied_counter), G
 				continue
 
 			rand_cand = random.choice(quorum_sets[cur_validator][1])
 			try_find_cand_counter = 0
-			while (rand_cand == cur_validator or len(list(nx.all_neighbors(G, rand_cand))) >= quorum_sets[rand_cand][0] + bump_ups or rand_cand in list(nx.all_neighbors(G, cur_validator))) and try_find_cand_counter < parameters.TRY_FIND_CAND_LIMIT:
+			while (rand_cand == cur_validator or rand_cand in list(nx.all_neighbors(G, cur_validator))) and try_find_cand_counter < parameters.TRY_FIND_CAND_LIMIT:
 				rand_cand = random.choice(quorum_sets[cur_validator][1])
 				try_find_cand_counter += 1
 
@@ -45,13 +44,20 @@ def alg1():
 
 		epoch_counter += 1
 		print("EPOCH: " + str(epoch_counter) + " " + "SATISFIED: " + str(satisfied_counter))
-		if epoch_counter > 10 and epoch_counter - last_bumpup_at > parameters.CAN_BUMP_UP:
-			bump_ups += 1
-			print("BUMPED!")
-			last_bumpup_at = epoch_counter
-
+		
 		all_validators = parameters.ALL_VALIDATORS.copy()
 
+def alg2_prune(G):
+	for i in range(0, parameters.TOTAL_NUM_OF_VALIDATORS-1):
+		for j in range(i+1, parameters.TOTAL_NUM_OF_VALIDATORS):
+			if G.has_edge(i, j) and len(list(nx.node_disjoint_paths(G, i, j))) >= 5:
+				G.remove_edge(i, j)
+	plt.figure(2)
+	nx.draw_networkx(G, with_labels=True)
+	plt.show()
+
 if __name__=="__main__": 
-    print(alg1())
+    [graph_form_str, G] = alg2_form()
+    print(graph_form_str)
+    alg2_prune(G)
 
